d3244bd7d7b6f46315ddf4d032461d985495df07
Brian Cristante <none>
2018-03-12 22:16:33 -0400

    Error handling in main, using exceptions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
+#include <string>
+
 #include "primes_in_range.hpp"
 
 using namespace std;
 
-int main()
+namespace
+{
+    struct usage_exception {};
+
+    //! Cast `int` to `unsigned int` or throw `invalid_argument` if it is negative
+    unsigned int to_uint(int n)
+    {
+        if (n < 0)
+        {
+            const auto msg = to_string(n) + " is negative.";
+            throw invalid_argument{ msg };
+        }
+
+        return static_cast<unsigned int>(n);
+    }
+}
+
+int main(int argc, char* argv[])
 {
     try
     {
-        const auto start = 0u;
-        const auto end = 10000u;
+        if (argc != 3)
+        {
+            throw usage_exception{};
+        }
+
+        const auto start_signed = stoi(argv[1]);
+        const auto start = to_uint(start_signed);
+
+        const auto end_signed = stoi(argv[2]);
+        const auto end = to_uint(end_signed);
+
         cout << "Primes in range [" << start << ", " << end << "]:" << endl;
         for (auto n : primes_in_range(start, end))
         {
@@ -17,8 +45,19 @@ int main()
 
         return 0;
     }
+    catch (usage_exception&)
+    {
+        cerr << "Usage: primes_in_range <begin> <end>" << endl;
+        return 1;
+    }
+    catch (invalid_argument& ia)
+    {
+        cerr << "[Error] " << ia.what() << endl;
+        return 1;
+    }
     catch (...)
     {
+        cerr << "[Error] an unknown error occurred" << endl;
         return -1;
     }
 }
